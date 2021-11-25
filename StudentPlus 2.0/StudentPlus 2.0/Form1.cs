047055b24e83672eede25bd5a5c3b02cf47d85a7
lilbitvoid <91071069+lilbitvoid@users.noreply.github.com>
2021-11-25 22:18:28 +0300

    Add files via upload

---
+++ StudentPlus 2.0/StudentPlus 2.0/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StudentPlus_2._0
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;
+        
+        private SqlDataAdapter sqlDataAdapter = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login = textBox1.Text;
+            string password = textBox1.Text;
+
+            if (login == "Admin" & password == "Admin")
+            {
+                Form s = new Form2();
+                s.Show();
+                this.Close();
+            }
+            else
+            {
+                try
+                {
+                    String loginUser = textBox1.Text;
+                    String passUser = textBox2.Text;
+
+                    sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ivan\source\repos\StudentPlus 2.0\StudentPlus 2.0\Database1.mdf;Integrated Security=True");
+                    sqlConnection.Open();
+
+                    DataTable table = new DataTable();
+
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+
+                    SqlCommand command = new SqlCommand("SELECT * FROM Users WHERE Username = @login AND Password = @password", sqlConnection);
+                    command.Parameters.Add("@login", SqlDbType.VarChar).Value = loginUser;
+                    command.Parameters.Add("@password", SqlDbType.VarChar).Value = passUser;
+                    sqlDataAdapter.SelectCommand = command;
+                    sqlDataAdapter.Fill(table);
+                    if (table.Rows.Count > 0)
+                    {
+                        this.Hide();
+                        Form f1 = new Form2();
+                        f1.ShowDialog();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Вы ввели неправильный логин или пароль!", "Ошибка ввода!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ivan\source\repos\StudentPlus 2.0\StudentPlus 2.0\Database1.mdf;Integrated Security=True");
+            sqlConnection.Open();
+        }
+    }
+}
---
+++ StudentPlus 2.0/StudentPlus 2.0/Form2.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace StudentPlus_2._0
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_ex = new System.Windows.Forms.Button();
+            this.btn_info = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
+            this.panel2.Controls.Add(this.btn_ex);
+            this.panel2.Controls.Add(this.btn_info);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.btn_1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(230, 451);
+            this.panel2.TabIndex = 6;
+            // 
+            // btn_ex
+            // 
+            this.btn_ex.AutoSize = true;
+            this.btn_ex.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_ex.FlatAppearance.BorderSize = 0;
+            this.btn_ex.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ex.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_ex.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_ex.Image = ((System.Drawing.Image)(resources.GetObject("btn_ex.Image")));
+            this.btn_ex.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_ex.Location = new System.Drawing.Point(3, 188);
+            this.btn_ex.Name = "btn_ex";
+            this.btn_ex.Size = new System.Drawing.Size(224, 54);
+            this.btn_ex.TabIndex = 11;
+            this.btn_ex.Text = "Экзамены";
+            this.btn_ex.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_ex.UseVisualStyleBackColor = true;
+            this.btn_ex.Click += new System.EventHandler(this.btn_ex_Click);
+            // 
+            // btn_info
+            // 
+            this.btn_info.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btn_info.AutoSize = true;
+            this.btn_info.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_info.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_info.FlatAppearance.BorderSize = 0;
+            this.btn_info.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_info.ForeColor = System.Drawing.Color.Black;
+            this.btn_info.Image = ((System.Drawing.Image)(resources.GetObject("btn_info.Image")));
+            this.btn_info.Location = new System.Drawing.Point(3, 393);
+            this.btn_info.Name = "btn_info";
+            this.btn_info.Size = new System.Drawing.Size(54, 54);
+            this.btn_info.TabIndex = 10;
+            this.btn_info.UseVisualStyleBackColor = true;
+            this.btn_info.Click += new System.EventHandler(this.btn_info_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(51)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 100);
+            this.panel1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("SimSun", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(42, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "StudentPlus";
+            // 
+            // btn_1
+            // 
+            this.btn_1.AutoSize = true;
+            this.btn_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_1.FlatAppearance.BorderSize = 0;
+            this.btn_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_1.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_1.Image = ((System.Drawing.Image)(resources.GetObject("btn_1.Image")));
+            this.btn_1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_1.Location = new System.Drawing.Point(3, 130);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(227, 54);
+            this.btn_1.TabIndex = 7;
+            this.btn_1.Text = "Отсутствующие студенты";
+            this.btn_1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(230, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(778, 410);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Sitka Heading", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Control;
+            this.label8.Location = new System.Drawing.Point(227, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(304, 42);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Расписание экзаменов";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(230, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(778, 41);
+            this.panel3.TabIndex = 10;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 451);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_info;
+        private System.Windows.Forms.Button btn_ex;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
